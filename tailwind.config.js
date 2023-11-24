7950e5fa3048ac98370f72193109c39029cc6e33
gabriribeira <gmrribeira@gmail.com>
2023-11-24 19:15:08 +0000

    Added Tailwind Configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,27 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "320px",
+      md: "640px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1600px",
+    },
+    display: ["group-hover"],
+    animation: {
+      wiggle: "wiggle 0.4s ease-in-out infinite",
+      spinner: "spin 3s linear infinite",
+    },
+    keyframes: {
+      wiggle: {
+        "0%, 100%": { transform: "rotate(-0.3deg)" },
+        "50%": { transform: "rotate(0.3deg)" },
+      },
+    },
   },
   plugins: [],
+  variants: {
+    scrollbar: ["rounded"],
+  },
 };
