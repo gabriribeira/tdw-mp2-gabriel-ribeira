9b8fe8fbf58ef070c6cd0e179517fc8714899a8a
gabriribeira <gmrribeira@gmail.com>
2023-11-25 15:19:52 +0000

    Added React Transition Group. Updated Navbar Responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Search from "./pages/Search";
+import Artist from "./pages/Artist";
+import Album from "./pages/Album";
+import Meems from "./pages/Meems";
+import Profile from "./pages/Profile";
 
 function App() {
   useEffect(() => {
@@ -11,6 +16,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/artist/:id" element={<Artist />} />
+        <Route path="/album/:id" element={<Album />} />
+        <Route path="/meems" element={<Meems />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CSSTransition } from "react-transition-group";
 
 const Navbar = () => {
   const location = useLocation();
+  const [mobileModal, setMobileModal] = useState(false);
   useEffect(() => {
     switch (location.pathname) {
       case "/":
@@ -23,11 +26,60 @@ const Navbar = () => {
     }
   }, [location.pathname]);
   return (
-    <nav className="w-screen sticky top-0 flex justify-center items-center bg-transparent text-[#2b2b2b] font-bold xl:text-[5rem] lg:text-[4rem] md:text-[3rem] text-[1.5rem] whitespace-pre z-[100] bg-white">
-      {location.pathname !== "/" && <Link to={"/"}>HOME .</Link>}
-      {location.pathname !== "/search" && <Link to={"/"}> SEARCH .</Link>}
-      {location.pathname !== "/meems" && <Link to={"/"}> MEEMS .</Link>}
-      {location.pathname !== "/profile" && <Link to={"/"}> PROFILE</Link>}
+    <nav className="w-screen sticky top-0 flex md:justify-center justify-between items-center bg-transparent text-[#2b2b2b] font-bold xl:text-[5rem] lg:text-[4rem] md:text-[3rem] text-[2rem] md:py-0 md:px-0 py-2 px-2 whitespace-pre z-[100] bg-white">
+      {location.pathname !== "/" && (
+        <Link to={"/"} className="md:block hidden">
+          HOME .
+        </Link>
+      )}
+      {location.pathname !== "/search" && (
+        <Link to={"/search"} className="md:block hidden">
+          {" "}
+          SEARCH .
+        </Link>
+      )}
+      {location.pathname !== "/meems" && (
+        <Link to={"/meems"} className="md:block hidden">
+          {" "}
+          MEEMS .
+        </Link>
+      )}
+      {location.pathname !== "/profile" && (
+        <Link to={"/profile"} className="md:block hidden">
+          {" "}
+          PROFILE
+        </Link>
+      )}
+      <button className="md:hidden block">MEEM</button>
+      <button
+        className="md:hidden block"
+        onClick={() => {
+          mobileModal ? setMobileModal(false) : setMobileModal(true);
+        }}
+      >
+        <GiHamburgerMenu />
+      </button>
+      <CSSTransition
+        in={mobileModal}
+        timeout={500}
+        classNames="menu-primary-vertical"
+        unmountOnExit
+      >
+        <div className="absolute md:hidden top-[100%] left-0 w-[100vw] h-auto bg-white z-[100] flex flex-col justify-center items-end px-2">
+          <Link to={"/"} className="my-2">
+            HOME
+          </Link>
+          <Link to={"/search"} className="my-2">
+            SEARCH
+          </Link>
+          <Link to={"/meems"} className="my-2">
+            MEEMS
+          </Link>
+          <Link to={"/profile"} className="my-2">
+            PROFILE
+          </Link>
+        </div>
+      </CSSTransition>
     </nav>
   );
 };
