707451c450c6bdd592520d71b18fea3c8099bc6d
gabriribeira <gmrribeira@gmail.com>
2023-11-28 19:36:16 +0000

    Styles Homepage Grid with Add To Modal

--- src/components/HomepageGrid.js
+++ src/components/HomepageGrid.js
@@ -3,12 +3,18 @@ import PropTypes from "prop-types";
 import { CiPlay1 } from "react-icons/ci";
 import { AiOutlinePause } from "react-icons/ai";
 import DefaultImage from "../assets/default.jpg";
+import { Link } from "react-router-dom";
+import { RxDotsHorizontal } from "react-icons/rx";
+import AddToModal from "./AddToModal";
 
 const HomepageGrid = (props) => {
   const data = props.data;
+  console.log(data);
   const [playing, setPlaying] = useState(false);
   const [track, setTrack] = useState(null);
   const audioRef = useRef(null);
+  const [trackModal, setTrackModal] = useState(null);
+
   function playTrack(track) {
     if (audioRef.current) {
       setTrack(track);
@@ -31,6 +37,7 @@ const HomepageGrid = (props) => {
       audioRef.current.pause();
     }
   }
+
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 relative">
       {data &&
@@ -39,11 +46,35 @@ const HomepageGrid = (props) => {
             key={item.track.id}
             className="flex flex-col col-span-1 group relative"
           >
-            <div className="group-hover:block hidden transition-all duration-[0.2s] absolute hidden w-full h-full p-5 z-[10]">
-              <div className="flex flex-col items-start font-bold text-2xl text-left">
-                <p>{item.track.name}</p>
-                <p>{item.track.artists[0].name}</p>
+            <div
+              className="group-hover:block hidden transition-all duration-[0.2s] absolute hidden w-full h-full p-3 z-[10]"
+              onMouseEnter={() => setTrackModal(null)}
+            >
+              <div className="flex flex-col items-start text-xl text-left">
+                <Link
+                  to={`/album/${item.track.album.id}`}
+                  className="uppercase font-bold cursor-pointer z-[100]"
+                >
+                  {item.track.name}
+                </Link>
+                <Link
+                  to={`/artist/${item.track.artists[0].id}`}
+                  className="uppercase cursor-pointer z-[100]"
+                >
+                  {item.track.artists[0].name}
+                </Link>
               </div>
+              <button
+                onClick={() =>
+                  trackModal ? setTrackModal(null) : setTrackModal(item.track)
+                }
+                className="absolute top-5 right-5 text-4xl z-[102]"
+              >
+                <RxDotsHorizontal />
+              </button>
+              {trackModal && trackModal.id == item.track.id && (
+                <AddToModal item={item.track} />
+              )}
               <div className="absolute w-full h-full flex justify-center items-center top-0 left-0">
                 {item.track.preview_url && (
                   <button
