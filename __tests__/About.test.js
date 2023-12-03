41b74fe7db216b76136258c4f38cd7f8dbb3296e
gabriribeira <gmrribeira@gmail.com>
2023-12-03 14:39:31 +0000

    Updated Tests

--- __tests__/About.test.js
+++ __tests__/About.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import About from "../pages/About";
 
 // eslint-disable-next-line no-undef
-test("Should have the word Tracks in the component", () => {
+test("Should have the word MEEM in the component", () => {
   render(<About />);
   const myElement = screen.getByText("MEEM");
   //eslint-disable-next-line no-undef
